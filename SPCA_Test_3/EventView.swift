8e5133817ae1239d03b988f1026251be1c9e2575
Zane Haubach <zhaubach@gmail.com>
2022-04-06 10:27:54 -0400

    Updated EventView

--- SPCA_Test_3/EventView.swift
+++ SPCA_Test_3/EventView.swift
@@ -1,75 +1,62 @@
 //
-//  EventView.swift
-//  SPCA_Test_3
+//  ContentView.swift
+//  WebTest
 //
-//  Created by Zane Haubach on 3/22/22.
+//  Created by Zane Haubach on 3/30/22.
 //
 
 import SwiftUI
-import HTMLKit
-import WebKit
+import SwiftSoup
 
 struct EventView: View {
-    @State var animate: Bool = true
-    @State var didLoad: Bool = false
-    init(){
-        let navbarAppearance = UINavigationBarAppearance()
-        navbarAppearance.backgroundColor = SPCABlueUI
-        navbarAppearance.shadowColor = .clear
-        UINavigationBar.appearance().standardAppearance = navbarAppearance
-        UINavigationBar.appearance().compactAppearance = navbarAppearance
-        UINavigationBar.appearance().scrollEdgeAppearance = navbarAppearance
-        UINavigationBar.appearance().tintColor = UIColor.white
-    }
+    var URLString = "https://winchesterspca.org/blog-2/events/"
     
-    let SPCABlue = Color(red: 33/255, green: 88/255, blue: 153/255)
-    let SPCABlueUI = UIColor(red: 33/255, green: 88/255, blue: 153/255, alpha: 1)
-    
+    @StateObject var eventViewer = EventViewer()
     var body: some View {
-        let url = "https://winchesterspca.org/blog-2/events/"
-        ZStack {
-            WebService(url: URL(string: url))
-            
-            //Splash Screen
-            
-            if(!didLoad){
-                ZStack{
-                    Color(UIColor.white)
-                    VStack {
-                        Text("Loading Events")
-                            .font(.headline)
-                            .foregroundColor(SPCABlue)
-                        
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle(tint:SPCABlue))
-                            .scaleEffect(2)
-                            .padding(.top, 40.0)
+        NavigationView{
+            ScrollView{
+                LazyVStack{
+                    ForEach(eventViewer.events, id: \.self){ event in
+                        VStack{
+
+                            URLImage(urlString: event.image)
+                                .frame(width: 440)
+                                .padding(.vertical)
+                            Text(event.eventName)
+                                .font(.title)
+                                .fontWeight(.black)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                                
+                            VStack{
+                                ForEach(event.details, id: \.self){ detail in
+                                    Text(detail)
+                                        .padding()
+                                        
+                                }
+                            }
+                        }
                     }
                 }
-                .edgesIgnoringSafeArea(.all)
-                .opacity(animate ? 1 : 0)
+                .onAppear{
+                    DispatchQueue.main.async{eventViewer.coordinateHTML(urlString: URLString)}
+                }
             }
-        }
-        .onAppear{
-            DispatchQueue.main.asyncAfter(deadline: .now()+2){
-                animate.toggle()
-                didLoad = true
-            }
-        }
-        .toolbar{
-            ToolbarItem(placement: .principal){
-                Image("SPCA_logo_top")
-                    .resizable()
-                    .frame(width: 165.0, height: 65)
-                    .ignoresSafeArea()
+            .toolbar{
+                ToolbarItem(placement: .principal){
+                    Image("SPCA_logo_top")
+                        .resizable()
+                        .frame(width: 165.0, height: 65)
+                        .ignoresSafeArea()
+                }
             }
         }
     }
-    
 }
 
-struct EventView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         EventView()
     }
 }
+
+
