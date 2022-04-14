778c488977eb5152ae24542487d0a343514d6e70
Zane Haubach <zhaubach@gmail.com>
2022-04-14 11:16:36 -0400

    Updated to build package

---
+++ Sources/SPCA_Test_3/SPCA_Test_3.swift
@@ -0,0 +1,6 @@
+public struct SPCA_Test_3 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SPCA_Test_3Tests/SPCA_Test_3Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SPCA_Test_3
+
+final class SPCA_Test_3Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SPCA_Test_3().text, "Hello, World!")
+    }
+}
